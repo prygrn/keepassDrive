fe7dc98efa75e53f52dcddaffd8b6e8560fce1f5
prygrn <pierremogrison@gmail.com>
2024-02-10 23:11:06 +0100

    Clear main

--- keepass.py
+++ keepass.py
@@ -1,78 +1,37 @@
+import sys
+import logging
 from pathlib import Path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
-SCOPES = [
-    "https://www.googleapis.com/auth/drive"
-]  # Check the API scopes to get the appropriate scope
+ARGUMENTS_NB = 3
 
-FILENAME = "Database.kdbx"
-TOKEN_FILE = "token.json"
-CREDENTIALS_FILE = "credentials.json"
+LOGGER = logging.getLogger(__name__)
 
+class WrongNumberOfArguments(Exception):
+    """
+    Raised when the module is not executed with the right number of arguments
+    """
+    
+    pass
 
 def main():
-    credentials = None
-    token_path = Path(TOKEN_FILE)
-    credentials_path = Path(CREDENTIALS_FILE)
+    current_directory = Path.cwd()
 
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if Path(token_path).is_file():
-        credentials = Credentials.from_authorized_user_file(str(token_path), SCOPES)
-    # If there are no (valid) credentials, let the user log in
-    if not credentials or not credentials.valid:
-        if credentials and credentials.expired and credentials.refresh_token:
-            credentials.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                str(credentials_path), SCOPES
-            )
-            credentials = flow.run_local_server(port=0)
-        # Save the token locally for the next run
-        with open(str(token_path), "w") as token:
-            token.write(credentials.to_json())
-
-    try:
-        service = build("drive", "v3", credentials=credentials)
-
-        # Call the Drive API
-        results = (
-            service.files()
-            .list(pageSize=10, fields="nextPageToken, files(id, name)")
-            .execute()
+    logging.basicConfig(
+        filename=f"{current_directory.name}.log",
+        filemode="w",
+        encoding="UTF-8",
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%d/%m/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+    
+    if len(sys.argv) != ARGUMENTS_NB:
+        LOGGER.critical(
+            """This tool requires the following arguments:
+                     - pThe name of the keepass database to be opened
+                     - The path/to/the/secrets/file"""
         )
-    except HttpError as error:
-        # Handle errors from Gmail API
-        print(f"An error occurred: {error}")
-        
-    items = results.get("files", [])
-
-    if not items:
-        print("No files found.")
-        print("Creating file")
-        create_file()
-        return
-
-    # Filter items on FILENAME
-    # TODO Try to filter through the API to reduce consumption and improve performance
-    file_metadata = [dict(name = item['name'], id = item['id']) for item in items if item['name'] == FILENAME]
-    if not file_metadata:
-        print("Creating file")
-        create_file()
-    else:
-        update_file(file_metadata[0])
-        print(f"{FILENAME} present, just need to be updated")
-
-def update_file(file_metadata):
-    pass
-
-def create_file():
-    pass
+        raise WrongNumberOfArguments("Invalid number of arguments")
 
 if __name__ == "__main__":
     main()
